c24599054a370c79f62a1135faa0f4bc05ecfc89
PurpleHP <ermertay@gmail.com>
2024-07-09 10:12:38 +0300

    cards changed

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import './Card.css';
 import Logo from '../assets/logo.svg';
+import { useNavigate } from 'react-router-dom';
 
-const Card = ({ children }) => {
+const Card = ({ children, to }) => { 
+    const navigate = useNavigate();
+
     return (
-        <div className="card">
-            <img src="src/assets/bank.png"
-                className="card-logo" alt='logo' />
+        <div className="card" onClick={() => navigate(to)}>
+             <img src="src/assets/bank.png" className="card-logo" alt='logo' />
             <div className="card-text">Yatırım</div>
-
             {children}
         </div>
     );
