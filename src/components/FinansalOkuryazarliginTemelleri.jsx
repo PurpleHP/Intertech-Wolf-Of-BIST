f15ba069fc61f9c2d4f1a32c52e66792277cfc23
PurpleHP <ermertay@gmail.com>
2024-07-10 12:42:09 +0300

    mark

--- src/components/FinansalOkuryazarliginTemelleri.jsx
+++ src/components/FinansalOkuryazarliginTemelleri.jsx
@@ -3,8 +3,11 @@ import Navbar from "./Navbar";
 import Card from "./Card";
 import { useState } from "react";
 
+//MARK: FinansalOkuryazarliginTemelleri
 function FinansalOkuryazarliginTemelleri(){
 
+    
+    //MARK: Paragraf Bölümü
     const allParagraphs = [
         "Ekonomide iki farklı aktör vardır. Fon arz edenler ve fon talep edenler. Fon arz edenler, ellerinde ihtiyaçlarından fazla fon bulunan kişiler iken, fon talep edenler ihtiyaç duydukları kadar fona sahip olmayanlardır. Bu kişiler arasında borç ya da ortaklık ilişkisinin doğması kaçınılmazdır. Gelin borç ilişkisine göz atalım. Borç isteyenlerin, bu borcu verebilecek kişileri bulması pek kolay değildir, bulsa bile şartlarda anlaşamayabilirler. Örneğin, yatırım yapmak isteyen bir kişi yatırımından ancak üç yıl sonra gelir elde etmeye başlayabilecek ve ancak üç yıl sonrasında geri ödeme yapabilecektir, ancak fon fazlası olan kişiler en fazla bir yıllığına borç vermek istiyor olabilir.",
         "Bir diğer önemli nokta da güven problemidir. Fon fazlası olan kişiler geri ödemenin zamanında ve tam olarak yapılmasını ister ancak bundan pek emin olamayacağı için borç vermek istemeyebilir. Bu durumda, birikim sahibi olmayan bireyler ihtiyaçları ve gelirleri arasındaki zamansal uyuşmazlıklar nedeniyle zorluk yaşayabilir veya yatırımlar için gerekli para bulunamayacağından ekonomik gelişim sağlanamayabilir.",
@@ -32,7 +35,6 @@ function FinansalOkuryazarliginTemelleri(){
                 <div className="col-span-3 grid grid-cols-10  grid-rows-10 p-4 m-4 bg-white h-[90vh]">
                 <p className="break-words font-bold	 text-justify	 rounded-md font-sans text-[28px] col-span-8 row-span-1 col-start-2 border-4 p-3  row-start-1">
                     Finansal Okuryazarlığın Temelleri</p>
-                    
                     <p className="break-words text-justify	 rounded-md font-sans text-[28px] col-span-8 row-span-8 col-start-2 border-4 p-3  row-start-2">
                     {paragraphs}</p>
                     
