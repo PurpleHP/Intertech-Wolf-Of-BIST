67b2deeaace007f27b64dc3edb889c1b4fb69ba7
Bora Toprak Yaşar <boratoprakyasar@hotmail.com>
2024-07-12 13:51:17 +0300

    added group logo

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,12 +3,14 @@ import NavbarComponent from './Navbar.jsx';
 import Menu from './Menu.jsx';
 import Card from './Card.jsx';
 import Footer from './Footer.jsx';
-
+import Kurt from "../assets/imageKurt.png";
 function About() {
     return (
         <div>
             <NavbarComponent></NavbarComponent>
             <div className="min-h-screen flex flex-col items-center justify-center bg-[#0B090A] p-8">
+                
+                <img src={Kurt} alt="" className="transform scale-50 -mb-20" />
             <div className="bg-[#161A1D] p-8 rounded-lg shadow-md w-11/12 md:w-2/3 lg:w-1/2 text-white">
                 <h2 className="text-2xl mb-6">Hakkımızda</h2>
                 <p className="mb-4">
