5af60154a2976b020763b75613fdfbb7acde906d
Bora Toprak Yaşar <boratoprakyasar@hotmail.com>
2024-07-25 12:40:01 +0300

    added quiz answers

--- src/components/Courses/KrediQuiz.jsx
+++ src/components/Courses/KrediQuiz.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 
-import useTestapi from './TestApi';
+import useTestApi from './TestApi';
 
 const FinansalOkuryazarliginTemelleriQuiz = () => {
 
