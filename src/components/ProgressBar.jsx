2138d33fd7f79d499d134b84343218357e29b196
Batu Kuscuoglu <kuscuoglu.batu@gmail.com>
2024-07-30 00:25:23 +0300

    progress-deneme

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -1,11 +1,9 @@
 import React, { useState, useEffect } from 'react';
 
 function CircularProgressBar() {
-
-
     const [progress, setProgress] = useState(0);
 
-    const radius = 70; 
+    const radius = 70;
     const stroke = 10;
     const normalizedRadius = radius - stroke * 2;
     const circumference = normalizedRadius * 2 * Math.PI;
@@ -13,52 +11,43 @@ function CircularProgressBar() {
 
     useEffect(() => {
         const fetchData = async () => {
-
             const storedUserId = localStorage.getItem('userId');
 
             try {
                 const raw = JSON.stringify({ "userId": storedUserId });
-        
+
                 const requestOptions = {
-                method: "POST",
-                headers: { 'Content-Type': 'application/json' },
-                body: raw,
-                redirect: "follow"
+                    method: "POST",
+                    headers: { 'Content-Type': 'application/json' },
+                    body: raw,
+                    redirect: "follow"
                 };
-                console.log(storedUserId)
+                console.log(storedUserId);
                 const targetUrl = 'https://financialtrainerfinal120240716125722.azurewebsites.net/api/Education/getEducationByUser';
                 const response = await fetch(targetUrl, requestOptions);
                 const data = await response.json();
+                let completedCount = 0;
                 for (let i = 0; i < data.length; i++) {
-                    if(data[i].status === "DONE"){
-                        IncreaseProgress();
+                    if (data[i].status === "DONE") {
+                        completedCount++;
                     }
-                    
                 }
+                const progressValue = (completedCount / 14) * 100;
+                setProgress(progressValue);
             } catch (error) {
                 console.error('Error:', error);
             }
         };
-    
-        fetchData();
-        
-      }, []);
 
-    const IncreaseProgress = () => {
-            let progress =progress + 100/14;
-            if(progress >= 98) {
-                progress = 100;
-            }
-            setProgress(progress);
-            return progress > 100 ? 100 : progress; 
-    }
+        fetchData();
+    }, []);
 
     return (
         <div className='flex h-screen w-screen justify-center items-center'>
             <svg height={radius * 2} width={radius * 2}>
-                    <circle
+                <circle
                     stroke={
-                        progress < 30 ? '#0f0' : progress < 70 ? '#f50' : progress == 100? '#0ff': '#f00'
+                        progress < 30 ? '#0f0' : progress < 70 ? '#f50' : progress === 100 ? '#0ff' : '#f00'
                     }
                     fill="transparent"
                     strokeWidth={stroke}
@@ -67,14 +56,13 @@ function CircularProgressBar() {
                     r={normalizedRadius}
                     cx={radius}
                     cy={radius}
-                    />
-                    <text x="50%" y="50%" textAnchor="middle" stroke="#51c5cf" dy=".3em">
+                />
+                <text x="50%" y="50%" textAnchor="middle" stroke="#51c5cf" dy=".3em">
                     {`${Math.floor(progress)}%`}
-                    </text>
-                </svg>
-                </div>
-      
+                </text>
+            </svg>
+        </div>
     );
-  }
-  
-  export default CircularProgressBar;
+}
+
+export default CircularProgressBar;
