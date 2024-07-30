2a91c1b1a1765d1999402f7f76ba6794c0cdc609
Batu Kuscuoglu <kuscuoglu.batu@gmail.com>
2024-07-30 21:45:11 +0300

    Home-Fix

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -34,6 +34,7 @@ import risk from "../assets/zor/risk.jpg";
 function App() {
   const [userId, setUserId] = useState(null);
   const [renderSite, setRenderSite] = useState(false);
+  const [notFinishedCourses, setNotFinishedCourses] = useState([]);
   const [formattedEducationIds, setFormattedEducationIds] = useState([
     { id: 1, isFinishedList: false },
     { id: 7, isFinishedList: false },
@@ -51,9 +52,22 @@ function App() {
     { id: 19, isFinishedList: false }
   ]);
 
-  const [notFinishedCourses, setNotFinishedCourses] = useState([]);
-
-  const educationIds = [1, 7, 4, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19];
+  const images = {
+    1: okuryazarlik,
+    7: bankacilikHizmetleri,
+    4: butceharcama,
+    9: kredikartlari,
+    10: tasarruf,
+    11: DebtImage,
+    12: kredi,
+    13: kisiselFinansalPlanlama,
+    14: VergiGelir,
+    15: paraSermayePiyasasi,
+    16: yatirimstrateji,
+    17: borsa,
+    18: kripto,
+    19: risk
+  };
 
   useEffect(() => {
     const fetchData = async () => {
@@ -72,28 +86,20 @@ function App() {
             const targetUrl = 'https://financialtrainerfinal120240716125722.azurewebsites.net/api/Education/getEducationByUser';
             const response = await fetch(targetUrl, requestOptions);
             const data = await response.json();
-            let twoCourse = 0;
-            let notFinished = [];
-
-            console.log("Data:\n", data)
-            for (let i = 0; i < data.length; i++) {
-              if (data[i].status === "DONE") {
-                data[i].eduId = parseInt(data[i].eduId);
-                console.log("Data[i].eduId: ", parseInt(data[i].eduId));
-                // Find the education object with the matching eduId
-                formattedEducationIds = formattedEducationIds.map(edu =>
-                  parseInt(edu.id) === parseInt(data[i].eduId) ? { ...edu, isFinishedList: true } : edu
-                );
-              } else if (twoCourse < 2) {
-                notFinished.push(data[i]);
-                twoCourse++;
-              }
-            }
             
-            setFormattedEducationIds(formattedEducationIds);
-            setNotFinishedCourses(notFinished);
-            console.log("Formatted:\n", formattedEducationIds)
-            console.log("Not Finished:\n", notFinished)
+            console.log("Data:\n", data);
+
+            const updatedFormattedEducationIds = formattedEducationIds.map(edu => {
+              const course = data.find(d => parseInt(d.eduId) === edu.id);
+              return course ? { ...edu, isFinishedList: course.status === "DONE" } : edu;
+            });
+
+            const notFinishedCourses = updatedFormattedEducationIds.filter(edu => !edu.isFinishedList).slice(0, 2);
+
+            setFormattedEducationIds(updatedFormattedEducationIds);
+            setNotFinishedCourses(notFinishedCourses);
+            console.log("Formatted:\n", updatedFormattedEducationIds);
+            console.log("Not Finished Courses:\n", notFinishedCourses);
             setRenderSite(true);
         } catch (error) {
             console.error('Error:', error);
@@ -108,117 +114,81 @@ function App() {
     setUserId(storedUserId);
   }, []);
 
-  const getCardImage = (eduId) => {
-    switch (eduId) {
-      case 1:
-        return okuryazarlik;
-      case 7:
-        return bankacilikHizmetleri;
-      case 4:
-        return butceharcama;
-      case 9:
-        return kredikartlari;
-      case 10:
-        return tasarruf;
-      case 11:
-        return DebtImage;
-      case 12:
-        return kredi;
-      case 13:
-        return kisiselFinansalPlanlama;
-      case 14:
-        return VergiGelir;
-      case 15:
-        return paraSermayePiyasasi;
-      case 16:
-        return yatirimstrateji;
-      case 17:
-        return borsa;
-      case 18:
-        return kripto;
-      case 19:
-        return risk;
-      default:
-        return okuryazarlik;
-    }
-  }
-
   return (
     <div className="App" id='home'>
       <NavbarComponent />
       {renderSite ? 
       <div>
         <Menu>
-          {userId && userId !== 0 ? (
-            <>
-              <div className="progress-header flex flex-col">
-                <h1 className='text-2xl mb-15'>İlerlemeniz</h1>
-                <div className="progress-bar-wrapper mt-5">
-                  <CircularProgressBar />
-                </div>
+        {userId && userId !== 0 ? (
+          <>
+            <div className="progress-header flex flex-col">
+              <h1 className='text-2xl mb-15'>İlerlemeniz</h1>
+              <div className="progress-bar-wrapper mt-5">
+                <CircularProgressBar />
               </div>
-              <div className='progress-cards'>
-                {notFinishedCourses.map((course, index) => (
-                  <Card 
-                    key={index} 
-                    cardName={course.courseName} 
-                    to={course.courseLink} 
-                    imgSrc={getCardImage(course.eduId)} 
-                    difficulty={course.difficulty} 
-                    EducationId={course.eduId}
-                  />
-                ))}
-              </div>
-            </>
-          ) : null}
-        </Menu>
-        <Menu>
-          <h1 className='text-2xl'>Tüm Kurslar</h1>
-          <div className='flex'>
-            <Card cardName="Finansal Okuryazarlık" to="/FinansalOkuryazarliginTemelleri" imgSrc={okuryazarlik} difficulty="easy" EducationId="1" 
-              isFinished={formattedEducationIds.find(item => item.id === 1)?.isFinishedList}/>
-            <Card cardName="Bankacılık Hizmetleri" to="/BankacilikHizmetleri" imgSrc={bankacilikHizmetleri} difficulty="easy" EducationId="7" 
-            isFinished={formattedEducationIds.find(item => item.id === 7)?.isFinishedList} />
-          </div>
-          <div className='flex'>
-            <Card cardName="Bütçe ve Harcama" to="/ButceveHarcama" imgSrc={butceharcama} difficulty="easy" EducationId="4" 
-            isFinished={formattedEducationIds.find(item => item.id === 4)?.isFinishedList}/>
-            <Card cardName="Kredi Kartları" to="/KrediKartlari" imgSrc={kredikartlari} difficulty="easy" EducationId="9"
-            isFinished={formattedEducationIds.find(item => item.id === 9)?.isFinishedList}/>
-          </div>
-          <div className='flex'>
-            <Card cardName="Tasarruf ve Acil Durum Fonları" to="/TasarrufveAcilDurumFonlari" imgSrc={tasarruf} difficulty="easy" EducationId="10"
-             isFinished={formattedEducationIds.find(item => item.id === 10)?.isFinishedList}/>
-            <Card cardName="Borç Yönetimi" to="/BorcYonetimi" imgSrc={DebtImage} difficulty="medium" EducationId="11" 
-            isFinished={formattedEducationIds.find(item => item.id === 11)?.isFinishedList}/>
-          </div>
-          <div className='flex'>
-            <Card cardName="Kredi" to="/Kredi" imgSrc={kredi} difficulty="medium" EducationId="12" 
-            isFinished={formattedEducationIds.find(item => item.id === 12)?.isFinishedList}/>
-            <Card cardName="Kişisel Finansal Planlama" to="/KisiselFinansalPlanlama" imgSrc={kisiselFinansalPlanlama} difficulty="medium" EducationId="13" 
-            isFinished={formattedEducationIds.find(item => item.id === 13)?.isFinishedList}/>
-          </div>
-          <div className='flex'>
-            <Card cardName="Gelir ve Vergi Yönetimi" to="/GelirveVergiYonetimi" imgSrc={VergiGelir} difficulty="medium" EducationId="14" 
-            isFinished={formattedEducationIds.find(item => item.id === 14)?.isFinishedList}/>
-            <Card cardName="Para ve Sermaye Piyasası" to="/ParaveSermayePiyasasi" imgSrc={paraSermayePiyasasi} difficulty="medium" EducationId="15" 
-            isFinished={formattedEducationIds.find(item => item.id === 15)?.isFinishedList}/>
-          </div>
-          <div className='flex'>
-            <Card cardName="Kişisel Yatırım Stratejisi" to="/KisiselYatirimStratejisi" imgSrc={yatirimstrateji} difficulty="hard" EducationId="16" 
-            isFinished={formattedEducationIds.find(item => item.id === 16)?.isFinishedList}/>
-            <Card cardName="Borsa" to="/Borsa" imgSrc={borsa} difficulty="hard" EducationId="17" 
-            isFinished={formattedEducationIds.find(item => item.id === 17)?.isFinishedList}/>
-          </div>
-          <div className='flex'>
-            <Card cardName="Kripto" to="/Kripto" imgSrc={kripto} difficulty="hard" EducationId="18" 
-            isFinished={formattedEducationIds.find(item => item.id === 18)?.isFinishedList} />
-            <Card cardName="Risk" to="/Risk" imgSrc={risk} difficulty="hard" EducationId="19" 
-            isFinished={formattedEducationIds.find(item => item.id === 19)?.isFinishedList}/>
-          </div>
-        </Menu>
+            </div>
+            <div className='progress-cards'>
+              {notFinishedCourses.map(course => (
+                <Card 
+                  key={course.id} 
+                  cardName={course.id in images ? course.id : `Kurs ${course.id}`} 
+                  to={`/course/${course.id}`} 
+                  imgSrc={images[course.id]} 
+                  difficulty="medium" 
+                />
+              ))}
+            </div>
+          </>
+        ) : null}
+      </Menu>
+      <Menu>
+        <h1 className='text-2xl'>Tüm Kurslar</h1>
+        <div className='flex'>
+          <Card cardName="Finansal Okuryazarlık" to="/FinansalOkuryazarliginTemelleri" imgSrc={okuryazarlik} difficulty="easy" EducationId="1" 
+            isFinished={formattedEducationIds.find(item => item.id === 1)?.isFinishedList}/>
+          <Card cardName="Bankacılık Hizmetleri" to="/BankacilikHizmetleri" imgSrc={bankacilikHizmetleri} difficulty="easy" EducationId="7" 
+          isFinished={formattedEducationIds.find(item => item.id === 7)?.isFinishedList} />
+        </div>
+        <div className='flex'>
+          <Card cardName="Bütçe ve Harcama" to="/ButceveHarcama" imgSrc={butceharcama} difficulty="easy" EducationId="4" 
+          isFinished={formattedEducationIds.find(item => item.id === 4)?.isFinishedList}/>
+          <Card cardName="Kredi Kartları" to="/KrediKartlari" imgSrc={kredikartlari} difficulty="easy" EducationId="9"
+          isFinished={formattedEducationIds.find(item => item.id === 9)?.isFinishedList}/>
+        </div>
+        <div className='flex'>
+          <Card cardName="Tasarruf ve Acil Durum Fonları" to="/TasarrufveAcilDurumFonlari" imgSrc={tasarruf} difficulty="easy" EducationId="10"
+           isFinished={formattedEducationIds.find(item => item.id === 10)?.isFinishedList}/>
+          <Card cardName="Borç Yönetimi" to="/BorcYonetimi" imgSrc={DebtImage} difficulty="medium" EducationId="11" 
+          isFinished={formattedEducationIds.find(item => item.id === 11)?.isFinishedList}/>
+        </div>
+        <div className='flex'>
+          <Card cardName="Kredi" to="/Kredi" imgSrc={kredi} difficulty="medium" EducationId="12" 
+          isFinished={formattedEducationIds.find(item => item.id === 12)?.isFinishedList}/>
+          <Card cardName="Kişisel Finansal Planlama" to="/KisiselFinansalPlanlama" imgSrc={kisiselFinansalPlanlama} difficulty="medium" EducationId="13" 
+          isFinished={formattedEducationIds.find(item => item.id === 13)?.isFinishedList}/>
+        </div>
+        <div className='flex'>
+          <Card cardName="Gelir ve Vergi Yönetimi" to="/GelirveVergiYonetimi" imgSrc={VergiGelir} difficulty="medium" EducationId="14" 
+          isFinished={formattedEducationIds.find(item => item.id === 14)?.isFinishedList}/>
+          <Card cardName="Para ve Sermaye Piyasası" to="/ParaveSermayePiyasasi" imgSrc={paraSermayePiyasasi} difficulty="medium" EducationId="15" 
+          isFinished={formattedEducationIds.find(item => item.id === 15)?.isFinishedList}/>
+        </div>
+        <div className='flex'>
+          <Card cardName="Kişisel Yatırım Stratejisi" to="/KisiselYatirimStratejisi" imgSrc={yatirimstrateji} difficulty="hard" EducationId="16" 
+          isFinished={formattedEducationIds.find(item => item.id === 16)?.isFinishedList}/>
+          <Card cardName="Borsa" to="/Borsa" imgSrc={borsa} difficulty="hard" EducationId="17" 
+          isFinished={formattedEducationIds.find(item => item.id === 17)?.isFinishedList}/>
+        </div>
+        <div className='flex'>
+          <Card cardName="Kripto" to="/Kripto" imgSrc={kripto} difficulty="hard" EducationId="18" 
+          isFinished={formattedEducationIds.find(item => item.id === 18)?.isFinishedList} />
+          <Card cardName="Risk" to="/Risk" imgSrc={risk} difficulty="hard" EducationId="19" 
+          isFinished={formattedEducationIds.find(item => item.id === 19)?.isFinishedList}/>
+        </div>
+      </Menu>
       </div>
-      : 
+        : 
       <div className="bg-black flex items-center justify-center w-screen h-screen">
           <img width={100} src={loadingGif} alt="Loading animation" />
       </div>}
