1ba679118b0a19183b4b10190d487a5df0114c45
PurpleHP <ermertay@gmail.com>
2024-07-30 19:51:38 +0300

    tick fix

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,6 +5,7 @@ import Card from './Card.jsx';
 import Footer from './Footer.jsx';
 import CircularProgressBar from './ProgressBar';
 import { useState, useEffect } from 'react';
+import loadingGif from '../assets/loading.gif';
 
 //--------------------------
 //MARK: Images
@@ -32,6 +33,7 @@ import risk from "../assets/zor/risk.jpg";
 
 function App() {
   const [userId, setUserId] = useState(null);
+  const [renderSite, setRenderSite] = useState(false);
   let [formattedEducationIds, setFormattedEducationIds] = useState([
     { id: 1, isFinishedList: false },
     { id: 7, isFinishedList: false },
@@ -95,6 +97,7 @@ function App() {
             
             setFormattedEducationIds(formattedEducationIds);
             console.log("Formatted:\n",formattedEducationIds)
+            setRenderSite(true);
         } catch (error) {
             console.error('Error:', error);
         }
@@ -112,8 +115,11 @@ function App() {
 
   return (
     <div className="App" id='home'>
+    
       <NavbarComponent />
-      <Menu>
+      {renderSite ? 
+      <div>
+        <Menu>
         {userId && userId !== 0 ? (
           <>
             <div className="progress-header flex flex-col">
@@ -134,33 +140,52 @@ function App() {
         <div className='flex'>
           <Card cardName="Finansal Okuryazarlık" to="/FinansalOkuryazarliginTemelleri" imgSrc={okuryazarlik} difficulty="easy" EducationId="1" 
             isFinished={formattedEducationIds.find(item => item.id === 1)?.isFinishedList}/>
-          <Card cardName="Bankacılık Hizmetleri" to="/BankacilikHizmetleri" imgSrc={bankacilikHizmetleri} difficulty="easy" EducationId="7" isFinished={formattedEducationIds.find(item => item.id === 1)?.isFinishedList} />
+          <Card cardName="Bankacılık Hizmetleri" to="/BankacilikHizmetleri" imgSrc={bankacilikHizmetleri} difficulty="easy" EducationId="7" 
+          isFinished={formattedEducationIds.find(item => item.id === 7)?.isFinishedList} />
         </div>
         <div className='flex'>
-          <Card cardName="Bütçe ve Harcama" to="/ButceveHarcama" imgSrc={butceharcama} difficulty="easy" EducationId="4" isFinished={true}/>
-          <Card cardName="Kredi Kartları" to="/KrediKartlari" imgSrc={kredikartlari} difficulty="easy" EducationId="9" isFinished={true}/>
+          <Card cardName="Bütçe ve Harcama" to="/ButceveHarcama" imgSrc={butceharcama} difficulty="easy" EducationId="4" 
+          isFinished={formattedEducationIds.find(item => item.id === 4)?.isFinishedList}/>
+          <Card cardName="Kredi Kartları" to="/KrediKartlari" imgSrc={kredikartlari} difficulty="easy" EducationId="9"
+          isFinished={formattedEducationIds.find(item => item.id === 9)?.isFinishedList}/>
         </div>
         <div className='flex'>
-          <Card cardName="Tasarruf ve Acil Durum Fonları" to="/TasarrufveAcilDurumFonlari" imgSrc={tasarruf} difficulty="easy" EducationId="10" isFinished={formattedEducationIds.find(item => item.id === 10)?.isFinishedList}/>
-          <Card cardName="Borç Yönetimi" to="/BorcYonetimi" imgSrc={DebtImage} difficulty="medium" EducationId="11" isFinished={formattedEducationIds.find(item => item.id === 11)?.isFinishedList}/>
+          <Card cardName="Tasarruf ve Acil Durum Fonları" to="/TasarrufveAcilDurumFonlari" imgSrc={tasarruf} difficulty="easy" EducationId="10"
+           isFinished={formattedEducationIds.find(item => item.id === 10)?.isFinishedList}/>
+          <Card cardName="Borç Yönetimi" to="/BorcYonetimi" imgSrc={DebtImage} difficulty="medium" EducationId="11" 
+          isFinished={formattedEducationIds.find(item => item.id === 11)?.isFinishedList}/>
         </div>
         <div className='flex'>
-          <Card cardName="Kredi" to="/Kredi" imgSrc={kredi} difficulty="medium" EducationId="12" isFinished={formattedEducationIds.find(item => item.id === 12)?.isFinishedList}/>
-          <Card cardName="Kişisel Finansal Planlama" to="/KisiselFinansalPlanlama" imgSrc={kisiselFinansalPlanlama} difficulty="medium" EducationId="13" isFinished={formattedEducationIds.find(item => item.id === 13)?.isFinishedList}/>
+          <Card cardName="Kredi" to="/Kredi" imgSrc={kredi} difficulty="medium" EducationId="12" 
+          isFinished={formattedEducationIds.find(item => item.id === 12)?.isFinishedList}/>
+          <Card cardName="Kişisel Finansal Planlama" to="/KisiselFinansalPlanlama" imgSrc={kisiselFinansalPlanlama} difficulty="medium" EducationId="13" 
+          isFinished={formattedEducationIds.find(item => item.id === 13)?.isFinishedList}/>
         </div>
         <div className='flex'>
-          <Card cardName="Gelir ve Vergi Yönetimi" to="/GelirveVergiYonetimi" imgSrc={VergiGelir} difficulty="medium" EducationId="14" isFinished={formattedEducationIds.find(item => item.id === 14)?.isFinishedList}/>
-          <Card cardName="Para ve Sermaye Piyasası" to="/ParaveSermayePiyasasi" imgSrc={paraSermayePiyasasi} difficulty="medium" EducationId="15" isFinished={formattedEducationIds.find(item => item.id === 15)?.isFinishedList}/>
+          <Card cardName="Gelir ve Vergi Yönetimi" to="/GelirveVergiYonetimi" imgSrc={VergiGelir} difficulty="medium" EducationId="14" 
+          isFinished={formattedEducationIds.find(item => item.id === 14)?.isFinishedList}/>
+          <Card cardName="Para ve Sermaye Piyasası" to="/ParaveSermayePiyasasi" imgSrc={paraSermayePiyasasi} difficulty="medium" EducationId="15" 
+          isFinished={formattedEducationIds.find(item => item.id === 15)?.isFinishedList}/>
         </div>
         <div className='flex'>
-          <Card cardName="Kişisel Yatırım Stratejisi" to="/KisiselYatirimStratejisi" imgSrc={yatirimstrateji} difficulty="hard" EducationId="16" isFinished={formattedEducationIds.find(item => item.id === 16)?.isFinishedList}/>
-          <Card cardName="Borsa" to="/Borsa" imgSrc={borsa} difficulty="hard" EducationId="17" isFinished={formattedEducationIds.find(item => item.id === 17)?.isFinishedList}/>
+          <Card cardName="Kişisel Yatırım Stratejisi" to="/KisiselYatirimStratejisi" imgSrc={yatirimstrateji} difficulty="hard" EducationId="16" 
+          isFinished={formattedEducationIds.find(item => item.id === 16)?.isFinishedList}/>
+          <Card cardName="Borsa" to="/Borsa" imgSrc={borsa} difficulty="hard" EducationId="17" 
+          isFinished={formattedEducationIds.find(item => item.id === 17)?.isFinishedList}/>
         </div>
         <div className='flex'>
-          <Card cardName="Kripto" to="/Kripto" imgSrc={kripto} difficulty="hard" EducationId="18" isFinished={formattedEducationIds.find(item => item.id === 18)?.isFinishedList} />
-          <Card cardName="Risk" to="/Risk" imgSrc={risk} difficulty="hard" EducationId="19" isFinished={formattedEducationIds.find(item => item.id === 19)?.isFinishedList}/>
+          <Card cardName="Kripto" to="/Kripto" imgSrc={kripto} difficulty="hard" EducationId="18" 
+          isFinished={formattedEducationIds.find(item => item.id === 18)?.isFinishedList} />
+          <Card cardName="Risk" to="/Risk" imgSrc={risk} difficulty="hard" EducationId="19" 
+          isFinished={formattedEducationIds.find(item => item.id === 19)?.isFinishedList}/>
         </div>
       </Menu>
+      </div>
+        : 
+      <div className="bg-black flex items-center justify-center w-screen h-screen">
+          <img width={100} src={loadingGif} alt="Loading animation" />
+      </div>}
+      
       <div id='contact'>
         <Footer />
       </div>
