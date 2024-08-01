46c85d1c67679cc75cdb4d9679d9b801a565d426
Batu Kuscuoglu <kuscuoglu.batu@gmail.com>
2024-08-01 10:00:19 +0300

    chatbot-minorfix-text2speech

--- src/components/Chatbot.jsx
+++ src/components/Chatbot.jsx
@@ -55,6 +55,9 @@ const ChatBot = () => {
 
     async function fetchTextToSpeech(text) {
         try {
+            const cleanedText = text.replace(/\n+/g, ' '); 
+            // Texti duzenlemek lazim cokul bosluklarda AI okumuyor
+
             const raw = JSON.stringify({ prompt: text });
             const requestOptions = {
                 method: "POST",
@@ -182,11 +185,13 @@ const ChatBot = () => {
                 const cleanedText = data.result
                 .replace(/(\s\s+|\n{3,})/g, function(match) {
                     if (match.includes('\n')) {
-                    return ' ';
+                    return '\n';
                     } else {
                     return ' ';
                     }
                 })
+                .replace(/\n{2,}/g, ' ') // fazla satirlarin sayisi iki olsun
+                .replace(/\s*\n\s*/g, '\n')
                 .replace(/\s{2,}/g, ' ') // fazla bosluklar tek bosluk olsun
                 .trim();
                 typeWriterEffect(cleanedText, newAiResponse);
