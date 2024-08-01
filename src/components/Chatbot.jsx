c773e9a833dc3b96048b1577a5ac66bf8fe72bc6
Batu Kuscuoglu <kuscuoglu.batu@gmail.com>
2024-08-01 10:08:32 +0300

    chatbot-text2speech-minorfix

--- src/components/Chatbot.jsx
+++ src/components/Chatbot.jsx
@@ -185,13 +185,13 @@ const ChatBot = () => {
                 const cleanedText = data.result
                 .replace(/(\s\s+|\n{3,})/g, function(match) {
                     if (match.includes('\n')) {
-                    return ' ';
+                    return '\n';
                     } else {
                     return ' ';
                     }
                 })
-                .replace(/\n{2,}/g, ' ') // fazla satirlarin sayisi iki olsun
                 .replace(/\s*\n\s*/g, '\n')
+                .replace(/\n{2,}/g, ' ') // fazla satirlarin sayisi iki olsun
                 .replace(/\s{2,}/g, ' ') // fazla bosluklar tek bosluk olsun
                 .trim();
                 typeWriterEffect(cleanedText, newAiResponse);
